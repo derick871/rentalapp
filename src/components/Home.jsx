c56623bc616b92f5cbb6c8f06fa08e1e63e6baad
derick871 <wanyamaderrick4@gmail.com>
2026-05-01 09:25:32 +0300

    create a component and home file on it

---
+++ src/components/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function home() {
+    
+  return (
+    <div>
+        <h1>Welcome to SpotHouse</h1>
+        <p>Discover the best spots in town with SpotHouse! 
+            Whether you're looking for a cozy café, a trendy restaurant, 
+            or a hidden gem, SpotHouse has got you covered. Our platform allows you to explore
+             and share your favorite spots with friends and fellow spot enthusiasts. Join our
+              community today and start discovering the best places around you!</p>
+       <img src="hero.png" alt="assets" />
+    </div>
+  )
+}
+
+export default home
